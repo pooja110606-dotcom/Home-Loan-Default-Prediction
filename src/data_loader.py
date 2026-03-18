a399113d718566a044d0160b52d6ba1a5cdd6154
Pooja Aganathan <pooja110606@gmail.com>
2026-03-18 15:46:17 +0530

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,1 +1,13 @@
+import pandas as pd
 
+def load_data():
+
+    application = pd.read_csv("data/raw/application_train.csv")
+    bureau = pd.read_csv("data/raw/bureau.csv")
+    bureau_balance = pd.read_csv("data/raw/bureau_balance.csv")
+    previous = pd.read_csv("data/raw/previous_application.csv")
+    installments = pd.read_csv("data/raw/installments_payments.csv")
+    credit = pd.read_csv("data/raw/credit_card_balance.csv")
+    pos = pd.read_csv("data/raw/POS_CASH_balance.csv")
+
+    return application, bureau, bureau_balance, previous, installments, credit, pos
